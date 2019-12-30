75a99776e29eeb2413f04942f8995f3199d3d212
JustinCheung6 <justincheung6@gmail.com>
2019-12-30 00:36:10 -0800

    -Created reticle -Player can now shoot bullets -Aiming needs rework

---
+++ FPSGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //Make sure the bullet has a rigidbody
+    private Rigidbody rb = null;
+
+    [SerializeField] private int durationTime = 5;
+    [SerializeField] private float moveSpeed = 5f;
+    [Tooltip("Set to the Layers of what the bullet needs to attack")]
+    [SerializeField] private LayerMask targetLayers = new LayerMask();
+
+    private void Awake()
+    {
+        StartCoroutine(TimeOut(durationTime));
+        rb = GetComponent<Rigidbody>();
+
+        //Velocity lets object move constantly
+        rb.velocity = transform.forward * moveSpeed;  
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        //Set the bullet's layer to the same as the object it spawns with
+        if (col.gameObject.layer == gameObject.layer)
+            return;
+
+        if (col.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            Destroy(col.gameObject);
+
+        //Make sure the object doesn't collide with anything when it spawns
+        Destroy(gameObject);
+    }
+
+    private IEnumerator TimeOut(int time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
---
+++ FPSGame/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform spawnPoint;
+
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+            Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+
+    }
+}
