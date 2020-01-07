cc42786edbfe7dc4d50c029e6edb1d4762e51bc4
JustinCheung6 <justincheung6@gmail.com>
2020-01-06 18:07:27 -0800

    Gave Enemies an injured state to show when they get shot by the player

--- FPSGame/Assets/Scripts/Enemy/EnemyHealth.cs
+++ FPSGame/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] private int health = 10;
+    [SerializeField] private GameObject enemyModelObject;
+    [SerializeField] private Material injuredMaterial;
+
+    private Material startMaterial;
+
+    private void Start()
+    {
+        startMaterial = enemyModelObject.GetComponent<Renderer>().material;
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
-
+        StartCoroutine(ChangeMaterial());
         if (health <= 0)
             Death();
     }
@@ -16,4 +25,12 @@ public class EnemyHealth : MonoBehaviour, IDamageable
     {
         Destroy(gameObject);
     }
+
+    private IEnumerator ChangeMaterial()
+    {
+        enemyModelObject.GetComponent<Renderer>().material = injuredMaterial;
+        yield return new WaitForSeconds(0.05f);
+        enemyModelObject.GetComponent<Renderer>().material = startMaterial;
+    }
+
 }
